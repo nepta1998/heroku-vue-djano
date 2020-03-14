4195edd08d2e4ff698cfc11d58abd73dee07cadf
nepta1998 <njpm28@gmail.com>
2020-03-14 17:58:15 -0400

    agregando front

---
+++ front_end/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    themeColor: '#8000ba',
+    name: 'PhoenixWare',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
+};
