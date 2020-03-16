2a01f5cf4413832a942f4968d9709cfd638deae9
nepta1998 <njpm28@gmail.com>
2020-03-16 00:00:00 -0400

    otro cambio.

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -8,7 +8,7 @@ import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
-//gajkhgjkadhjkadgjkhkjgadhjdagfsfssfhsfhf
+
 new Vue({
   router,
   store,
