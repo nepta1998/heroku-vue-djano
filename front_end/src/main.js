80f6e819f32732729d1f0808d38781d113a265b8
nepta1998 <njpm28@gmail.com>
2020-03-15 23:29:19 -0400

    agregando vuetify.

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -8,7 +8,7 @@ import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
-//hola
+//gajkhgjkadhjkadgjkhkjgadhjdag
 new Vue({
   router,
   store,
